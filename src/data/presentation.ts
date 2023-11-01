8ce30d4f85fdc0262d8515f7f14e9839201af633
Slimane <80587317+slimanesedrati@users.noreply.github.com>
2023-11-01 19:21:10 +0100

    update contact email

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -14,7 +14,7 @@ const presentation: PresentationType = {
   title: "Hi, I'am Slimane",
   description:
     "Enthusiastic software engineering student and front-end developer from Algeria, deeply engaged in the world of NextJS, TypeScript, and IoT. Curious about AI and ML, I'm passionate about creating amazing web experiences and exploring the potential of IoT. Let's collaborate to craft exceptional web and IoT solutions! 🚀💫🌐",
-  mail: "Slimanesedrati2000@gmail.com",
+  mail: "slimane.sedrati@outlook.com",
   social: [
     { label: "Github", url: "https://github.com/Slimanesedrati" },
     { label: "𝕏", url: "https://x.com/Slimanesedrati" },
