cf244927ba3d44f4d943635ca583717819ed4d7c
Slimane <80587317+slimanesedrati@users.noreply.github.com>
2023-11-01 16:44:02 +0100

    update favicon

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -13,7 +13,7 @@ type PresentationType = {
 const presentation: PresentationType = {
   title: "Hi, I'am Slimane",
   description:
-    "Passionate software engineering student and frontend developer from Algeria. Currently working with NextJS and Typescript. Curious about AI and ML. Let's create amazing web experiences together! 🚀💫",
+    "Enthusiastic software engineering student and front-end developer from Algeria, deeply engaged in the world of NextJS, TypeScript, and IoT. Curious about AI and ML, I'm passionate about creating amazing web experiences and exploring the potential of IoT. Let's collaborate to craft exceptional web and IoT solutions! 🚀💫🌐",
   mail: "Slimanesedrati2000@gmail.com",
   social: [
     { label: "Github", url: "https://github.com/Slimanesedrati" },
